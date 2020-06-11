f9bed4d77e84989840d6915949af5b17d915da61
michalikovak <klaramichalikova@gmail.com>
2020-06-11 11:12:18 +0200

    1.commit

---
+++ components/Logo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <img src="images/Logo.png" alt="Logo Fox" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Logo"
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div>Funguj prosim</div>
+    <button>ahoj</button>
+    <img src=assets/Logo.png alt="Fox logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  background: linear-gradient(#ffffff, #ffd788);
+  width: 100vh;
+  height: 100vh;
+}
+
+#app button {
+  background-color: #7e3b1c;
+  color: white;
+  border-radius: 6px;
+  border: 2px solid #7e3b1c;
+}
+</style>
