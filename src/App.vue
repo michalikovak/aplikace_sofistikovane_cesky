b754a5b08450e12f065cd557a111eb6bc5740b12
michalikovak <klaramichalikova@gmail.com>
2020-06-12 18:44:41 +0200

    oprava

--- src/App.vue
+++ src/App.vue
@@ -67,16 +67,9 @@ export default {
 </script>
 
 <style>
-* {
+#app {
   background: linear-gradient(#ffffff, #ffd788);
   width: 100vh;
   height: 100vh;
-}
-
-#app button {
-  background-color: #7e3b1c;
-  color: white;
-  border-radius: 6px;
-  border: 2px solid #7e3b1c;
 }
 </style>
