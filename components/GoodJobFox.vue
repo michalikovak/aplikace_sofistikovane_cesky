2700880598f08418d5c634cef2253fc7e48685c4
gabistebelova <stebelkag@seznam.cz>
2020-06-20 17:05:00 +0200

    GoodJobFox

--- components/GoodJobFox.vue
+++ components/GoodJobFox.vue
@@ -1,22 +1,37 @@
 <template>
-  <div>
-    <img :src="resultsFox" alt="ResultFox" class="resultFox" />
+  <div id="kontejnerResults">
+    <BublinaSTextem2 class="BublinaSTextem2" />
+    <resultsFox class="resultsFox" />
+
     <img id="BublinaSTextem2" :src="BublinaSTextem2" alt="Text Bubble" />
+    <img :src="resultsFox" alt="ResultFox" class="resultFox" />
+    <p>Dobře 9/10</p>
   </div>
 </template>
 
 <script>
-import resultsFox from "../assets/img/resultsFox.png";
 import BublinaSTextem2 from "../assets/img/BublinaSTextem2.png";
+import resultsFox from "../assets/img/resultsFox.png";
 export default {
   name: "GoodJobFox",
   data() {
     return {
-      resultsFox,
       BublinaSTextem2,
+      resultsFox,
     };
   },
 };
 </script>
 
-<style></style>
+<style>
+#kontejnerResults {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+p {
+  font-size: 1.2rem;
+}
+</style>
